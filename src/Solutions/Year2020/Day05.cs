fb640ad1ad5e4f96cff309f375e5a0fb78bdb394
Jack <jacksavage@users.noreply.github.com>
2020-12-14 21:16:20 -0800

    Fixes interpretation of surrounding seat reqs

--- src/Solutions/Year2020/Day05.cs
+++ src/Solutions/Year2020/Day05.cs
@@ -13,7 +13,7 @@ namespace AdventOfCode.Solutions.Year2020
                 1 => input.Select(line => SeatId(line, c1, r1)).Max().ToString(),
                 2 =>
                     FindSeat(
-                        seatIDs:input.Select(line => SeatId(line, c1, r1)).ToHashSet(),
+                        seatIDs:input.Select(line => SeatId(line, c1, r1)),
                         c1, r1
                     ).ToString(),
                 _ => null
@@ -36,24 +36,12 @@ namespace AdventOfCode.Solutions.Year2020
         static (int y0, int y1) Partition(int x0, int x1, bool lowerHalf) =>
             lowerHalf ? (x0, x0 + (x1 - x0) / 2) : (x0 + (x1 - x0) / 2 + 1, x1);
     
-        static int FindSeat(HashSet<int> seatIDs, int c1, int r1)
+        static int FindSeat(IEnumerable<int> seatIDs, int c1, int r1)
         {
-            for (int c = 0; c <= c1; c++)
+            int[] buffer = seatIDs.OrderBy(s => s).ToArray();
+            for (int i = 0; i < buffer.Length - 1; i++)
             {
-                for (int r = 0; r <= r1; r++)
-                {
-                    static int seatID(int rr, int cc) => rr * 8 + cc;
-                    bool seatFound(int rr, int cc) => seatIDs.Contains(seatID(rr, cc));
-
-                    bool mySeat =
-                        seatFound(r, c) &&
-                        (c == 0 || seatFound(r, c - 1)) &&
-                        (c == c1 || seatFound(r, c + 1)) &&
-                        (r == 0 || seatFound(r - 1, c)) &&
-                        (r == r1 || seatFound(r + 1, c));
-                    
-                    if (mySeat) return seatID(r, c);
-                }
+                if (buffer[i + 1] == buffer[i] + 2) return buffer[i] + 1;
             }
 
             return -1;
